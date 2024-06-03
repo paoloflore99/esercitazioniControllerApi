9d29b2f5467fa6f118916c6be082361e0dec6a75
paolo flore <paoloflore937@gmail.com>
2024-06-03 23:57:24 +0200

    collegamento al db

---
+++ esercitazioniControllerApi/Data/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
+
+namespace esercitazioniControllerApi.Data
+{
+    public class DbContext : IdentityDbContext<IdentityUser>
+    {
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=Foto;" +
+                "Integrated Security=True;TrustServerCertificate=True");
+        }
+    }
+}
